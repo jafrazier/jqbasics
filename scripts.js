b6d07ff138be8e98eb2e9bca990ff40e49dfa45e
jfrazier <johnfrazier1222@gmail.com>
2017-05-01 18:40:14 -0400

    minichal

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$('body').hide();
+
+if (confirm('click here')) {
+    $('body').show();
+};
+
+$(".monkey").click( function(){     
+	alert('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');   
+} );
+
+$(".little").click( function(){     
+	alert('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');   
+} );
+
+$(".monkey, .little").hide();
+$(".monkey, .little").fadeIn(4000);
+
+$("p").hide();	
+$("p").slideDown(2000);
+
+
